62d8709f37f0af149f6904e3eb1711101a77d127
Student <macieksopata@vp.pl>
2019-12-08 22:10:48 +0100

    done

--- 08_fake_mock_or_stub/test/include/ObserverMock.h
+++ 08_fake_mock_or_stub/test/include/ObserverMock.h
@@ -4,6 +4,7 @@
 #include "../../include/Observer.h"
 
 class ObserverMock : public Observer{
+public:
     MOCK_METHOD2(remember, void(std::string question, std::string answer));
     MOCK_CONST_METHOD1(answer, std::string(std::string question));
 };
--- 08_fake_mock_or_stub/test/src/UniverseTest.cpp
+++ 08_fake_mock_or_stub/test/src/UniverseTest.cpp
@@ -34,20 +34,40 @@ public:
 
 TEST(Universe, Create)
 {
-    StrictMock<MyMock> MyMock{};
-    Dummy dummy{MyMock};
-    EXPECT_CALL(MyMock, test()).WillOnce(Return(10));
-    EXPECT_EQ(10, dummy.execute());
+    StrictMock<TimeMock> time;
+    StrictMock<SpaceMock> space;
+    StrictMock<ObserverMock> observer;
 
-    TimeImpl myTime;
+    Universe universe(time, space, observer);
 
-    StrictMock<TimeMock> timeMock{};
-    StrictMock<SpaceMock> spaceMock{};
-    StrictMock<ObserverMock> observerMock{};
-
-    Universe universe = Universe( timeMock, spaceMock, observerMock);
-    EXPECT_CALL(spaceMock, create(11));
+    EXPECT_CALL(time, create(true));
+    EXPECT_CALL(space, create(11));
+    EXPECT_CALL(space, dimensions()).WillOnce(Return(10));
+    EXPECT_CALL(observer, remember("How many dimensions there are?", std::to_string(10)));
     universe.create();
 
+}
 
+TEST(Universe, Simulate)
+{
+    StrictMock<TimeMock> time;
+    StrictMock<SpaceMock> space;
+    StrictMock<ObserverMock> observer;
+
+    Universe universe(time, space, observer);
+
+    EXPECT_CALL(time, create(true));
+    EXPECT_CALL(space, create(11));
+    EXPECT_CALL(space, dimensions()).WillOnce(Return(11));
+    EXPECT_CALL(observer, remember("How many dimensions there are?", std::to_string(11)));
+
+    universe.create();
+
+    EXPECT_CALL(time, now()).WillOnce(Return(9300000000)).WillOnce(Return(9900000000)).WillOnce(Return(13800000000)).WillOnce(Return(15000000000));
+    EXPECT_CALL(time, flow()).Times(3);
+    EXPECT_CALL(observer, remember("Is there planet Earth?", "Yes!"));
+    EXPECT_CALL(observer, remember("Does life exist?", "Yes!"));
+    EXPECT_CALL(observer, remember("Have People evolved?", "Yes!"));
+
+    universe.simulate( 14999999999);
 }
